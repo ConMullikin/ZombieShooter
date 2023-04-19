0a7003f6a29259bd9bc864cf166f4e10e8e38b9c
ConMullikin <mullikincp22@gcc.edu>
2023-04-19 19:02:04 -0400

    Finished javadoc'ing entities

--- src/Entities.java
+++ src/Entities.java
@@ -2,8 +2,17 @@
  * Abstract class that provides the basis for Player and Zombie objects
  */
 public abstract class Entities extends GeneralStats {
+    /**
+     * Keeps track of moves done by entity in their turn
+     */
     private int moves;
+    /**
+     * Number of lives that entity has left
+     */
     private int lives;
 
+    /**
+     * Follows through for the entity's move for that turn
+     */
     public abstract void makeTurn();
 }
