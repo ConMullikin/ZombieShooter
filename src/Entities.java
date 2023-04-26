23d81882d276400637e58ac5e84d1fe211efb9ed
ConMullikin <mullikincp22@gcc.edu>
2023-04-25 20:47:46 -0400

    Fixed BoardPrinter and modified coords so that it's in Entities and only uses 1-D matrice

--- src/Entities.java
+++ src/Entities.java
@@ -10,9 +10,19 @@ public abstract class Entities extends GeneralStats {
      * Number of lives that entity has left
      */
     private int lives;
+    /**
+     * Tracks the position of the entity in the 2D array
+     */
+    private int[] coordinates;
 
     /**
      * Follows through for the entity's move for that turn
      */
     public abstract void makeTurn();
+
+    /**
+     * Getter which returns the entity's position in the 2-D array
+     * @return the coordinates of the entity's position in the matrix
+     */
+    public int[] getCoords() { return coordinates; }
 }
